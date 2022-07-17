545b9fcf22be5480e04c8188e7dcbc7c7fe060b5
cirel.ramos <cirel.ramos@litermi.com>
2022-07-16 20:51:32 -0400

    feat(schedulers, services): change string to set disable query

--- src/Schedulers/CustomCommand.php
+++ src/Schedulers/CustomCommand.php
@@ -35,7 +35,7 @@ class CustomCommand extends Command
     {
         $disableQuery = $this->argument('disable_query');
         if (empty($disableQuery) === false && $disableQuery !== 'record_active') {
-            request()->headers->set('j0ic3-disable-4ZZm4uG-0a7P1-query-PiEcPBU', 1);
+            request()->headers->set('j0ic3-disable-4ZZm4uG-0a7P1-query-PiEcPBU', "disable-query");
         }
     }
 }
--- src/Services/DisableActiveRecordService.php
+++ src/Services/DisableActiveRecordService.php
@@ -12,7 +12,7 @@ class DisableActiveRecordService
     public static function execute($disableQuery): void
     {
         if (empty($disableQuery) === false && $disableQuery !== 'record_active') {
-            request()->headers->set('j0ic3-disable-4ZZm4uG-0a7P1-query-PiEcPBU', 1);
+            request()->headers->set('j0ic3-disable-4ZZm4uG-0a7P1-query-PiEcPBU', "disable-query");
         }
     }
 }
