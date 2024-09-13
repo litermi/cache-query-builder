94de89144996ca269b32b84b2852dff5491f4cef
cirel <cirel.ramos@litermi.com>
2024-09-13 10:21:15 -0400

    feat(composer): use values of pagination to get global cache part 2

--- src/Services/GetCacheService.php
+++ src/Services/GetCacheService.php
@@ -4,6 +4,7 @@ namespace Litermi\Cache\Services;
 
 use Exception;
 use Illuminate\Support\Facades\Cache;
+use Litermi\Cache\Classes\CacheConst;
 
 /**
  * Class GetCacheService
@@ -21,6 +22,7 @@ class GetCacheService
     {
         $tag       = GetTagCacheService::execute(null, $tag);
         $customKey = GetKeyCacheBySystemService::execute($customKey);
+        $customKey = self::getDataFromPagination($customKey);
         $headerName = config('cache-query.header_force_not_cache_name');
         $headerName = empty($headerName) ?  'force-not-cache' : $headerName;
         if (request()->header($headerName) != null) {
@@ -29,4 +31,23 @@ class GetCacheService
         return Cache::tags($tag)
             ->get($customKey);
     }
+
+    /**
+     * @param $customKey
+     * @return mixed|string
+     */
+    private static function getDataFromPagination($customKey)
+    {
+
+        [$perPage, $page] = GetParametersPaginationService::execute();
+        if ($page != CacheConst::PAGE) {
+            $customKey .= '_' . $page;
+        }
+        if ($perPage != CacheConst::PER_PAGE) {
+            $customKey .= '_' . $perPage;
+        }
+        return $customKey;
+    }
+
+
 }
