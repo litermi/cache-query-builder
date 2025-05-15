e2dca5096e459dc0fd32656ff9a4854882c16ff9
cirelramos <cirel.ramos@litermi.com>
2025-05-15 14:52:27 -0400

    feat(cache, facade): added and used new Facade connection to accept different connections part 5

--- src/Services/CacheCustomService.php
+++ src/Services/CacheCustomService.php
@@ -34,6 +34,16 @@ class CacheCustomService
     {
     }
 
+    public function put($key, $value, $time)
+    {
+        if (env('CACHE_DRIVER') != 'redis') {
+            Cache::put($key, $value, $time);
+        }
+        if (env('CACHE_DRIVER') == 'redis') {
+            Cache::tags($this->tag)->put($key, $value, $time);
+        }
+    }
+
     /**
      * @return $this
      */
@@ -43,61 +53,52 @@ class CacheCustomService
         return $this;
     }
 
-    public function get($key)
-    {
-        if(env('CACHE_DRIVER')!='redis') {
-            return Cache::get($key);
-        }
-        if(env('CACHE_DRIVER')=='redis') {
-            return Cache::tags($this->tag)->get($key);
-        }
-    }
-
-    public function put($key, $value, $time)
-    {
-        if(env('CACHE_DRIVER')!='redis'){
-            Cache::put($key, $value, $time);
-        }
-        if(env('CACHE_DRIVER')=='redis'){
-            Cache::tags($this->tag)->put($key, $value, $time);
-        }
-    }
-
     public function flush()
     {
-        if(env('CACHE_DRIVER')!='redis'){
+        if (env('CACHE_DRIVER') != 'redis') {
             Cache::flush();
         }
-        if(env('CACHE_DRIVER')=='redis'){
+        if (env('CACHE_DRIVER') == 'redis') {
             Cache::tags($this->tag)->flush();
         }
     }
 
     public function forget($key)
     {
-        if(env('CACHE_DRIVER')!='redis') {
-            Cache::get($key);
+        if (env('CACHE_DRIVER') != 'redis') {
+            Cache::forget($key);
         }
-        if(env('CACHE_DRIVER')=='redis') {
+        if (env('CACHE_DRIVER') == 'redis') {
             Cache::tags($this->tag)->forget($key);
         }
     }
 
     public function remember($key, $ttl, Closure $callback)
     {
-        if(env('CACHE_DRIVER')!='redis') {
-            Cache::remember($key,$ttl,$callback);
+        if (env('CACHE_DRIVER') != 'redis') {
+            return Cache::remember($key, $ttl, $callback);
         }
-        if(env('CACHE_DRIVER')=='redis') {
-            Cache::tags($this->tag)->remember($key,$ttl,$callback);
+        if (env('CACHE_DRIVER') == 'redis') {
+            return Cache::tags($this->tag)->remember($key, $ttl, $callback);
         }
     }
+
     public function has($key)
     {
-        if(env('CACHE_DRIVER')!='redis') {
+        if (env('CACHE_DRIVER') != 'redis') {
             return Cache::get($key);
         }
-        if(env('CACHE_DRIVER')=='redis') {
+        if (env('CACHE_DRIVER') == 'redis') {
+            return Cache::tags($this->tag)->get($key);
+        }
+    }
+
+    public function get($key)
+    {
+        if (env('CACHE_DRIVER') != 'redis') {
+            return Cache::get($key);
+        }
+        if (env('CACHE_DRIVER') == 'redis') {
             return Cache::tags($this->tag)->get($key);
         }
     }
